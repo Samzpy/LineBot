518ca08f44d546b7d7467dd145d593a1a3a76c99
ssa <ss@yahoo.com>
2020-09-22 16:36:45 +0800

    'lp'

--- app.py
+++ app.py
@@ -36,14 +36,17 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     # message = TextSendMessage(text=event.message.text)
-    if "資訊" == event.message.text:
-        DcardCrawler=crawler()
-        result= DcardCrawler.information
+    if event.message.text[0] == '0'
+        if "資訊" == event.message.text:
+            DcardCrawler=crawler()
+            result= DcardCrawler.information
+        else:
+            DcardCrawler=crawler()
+            result=DcardCrawler.crawl_specific_forum(event.message.text)
+        message = TextSendMessage(text=result)
+        line_bot_api.reply_message(event.reply_token, message)
     else:
-        DcardCrawler=crawler()
-        result=DcardCrawler.crawl_specific_forum(event.message.text)
-    message = TextSendMessage(text=result)
-    line_bot_api.reply_message(event.reply_token, message)
+        
 
 import os
 if __name__ == "__main__":
